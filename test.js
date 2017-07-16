6ebe328960d7d234f47088c3b0c0b4951a9cdb61
Fabio Nolasco <gomidefabio@gmail.com>
2017-07-16 19:51:05 -0400

    Refacted JavaScript

--- test.js
+++ test.js
@@ -1,32 +1,24 @@
-var list = document.getElementById('addItem');
-
-function changeText2() {
-	var item = document.getElementById("item").value;
-	var entry = document.createElement("option");
-	entry.appendChild(document.createTextNode(item));
-	list.appendChild(entry);
-	var currentSize = $("#addItem").attr("size");
-	currentSize++;
-	$("#addItem").attr("size", currentSize);
-}
-
-$('#reset-button').click(function() {
-	$(':input', '#cblist')
-		.not(':button, :submit, :reset, :hidden')
-		.val('')
-		.removeAttr('checked')
-		.removeAttr('selected');
-
-	$("#cblist").empty();
-});
-
+window.changeText2 = function() {};
 
 $(document).ready(function() {
- 
-	$('#push2list').click(function(){
-		$('#addItem option:selected').each( function() {
-			$('#checklist').append("<input value='"+$(this).val()+"'>"+$(this).text()+"</input>");  
-			addCheckbox($('#addItem').val());
+	window.changeText2 = function() {
+		var list = document.getElementById('addItem');
+		var item = document.getElementById('item').value;
+		var entry = document.createElement('option');
+		entry.appendChild(document.createTextNode(item));
+		list.appendChild(entry);
+		var currentSize = $('#addItem').attr('size');
+		$('#addItem').attr('size', ++currentSize);
+	};
+
+	$('#reset-button').click(function() {
+		$('input', '#cblist').not('button, submit, reset, hidden').val('').removeAttr('checked').removeAttr('selected');
+		$('#cblist').empty();
+	});
+
+	$('#push2list').click(function() {
+		$('#addItem option:selected').each(function(index, element) {
+			addCheckbox($(element).text());
 			$('#cblist').append($('<br/>'));
 		});
 	});
@@ -34,29 +26,7 @@ $(document).ready(function() {
 	function addCheckbox(name) {
 		var container = $('#cblist');
 		var inputs = container.find('input');
-
-		$('<input />', {type: 'checkbox', id: 'cb', value: name }).appendTo(container);
-		$('<label />', {'for': 'cb', text: name }).appendTo(container);
-	}                   
+		$('<input />', { type: 'checkbox', id: 'cb', value: name }).appendTo(container);
+		$('<label />', { for: 'cb', text: name }).appendTo(container);
+	}
 });
-
-
-
-/*$('#reset-button').click(function() {
-        $('#checklist option:selected').each( function() {
-            $('#checklist').remove("<option value='"+$(this).val()+"'>"+$(this).text()+"</option>");
-            $(this).remove();
-        });
-    }); */
-
-/*function add2list() {
-	var itm = document.getElementById("addItem").lastChild;
-	var cln = itm.cloneNode(true);
-	document.getElementById("checklist").appendChild(cln);
-}
-*/
-
-/*
-var count = $("addItem :selected").length;
-$("addItem").attr('size', count);
-*/
